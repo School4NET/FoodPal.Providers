9be5a716812be57be1acc28fc40bc7b775e73f32
Diana Constantinescu <diana.constantinescu@sorted.com>
2021-02-11 12:59:30 +0200

    WIP added CatalogueItemRepo and Postman collection

---
+++ src/FoodPal.Providers.DataAccess/Repository/ICatalogueItemsRepository.cs
@@ -0,0 +1,12 @@
+﻿using FoodPal.Providers.DomainModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FoodPal.Providers.DataAccess.Repository
+{
+    public interface ICatalogueItemsRepository
+    {
+        Task<IEnumerable<CatalogueItem>> GetAllWithProviderAsync(int providerId);
+        Task<CatalogueItem> GetWithProviderByIdAsync(int catalogueItemId);
+    }
+}
